8cd1c227af7af00e3298374c3ded76a61b53ec72
Ania <ania54345@gmail.com>
2025-01-11 18:06:53 +0100

    Prevent crashes when an image is not fully saved

--- translator.py
+++ translator.py
@@ -42,16 +42,29 @@ first = True
 while True:
 	if len(os.listdir(image_path)) > 0:
 		
-		time.sleep(.1)
+		# Images which are not yet fully saved will raise errors:
+		
+		# PIL.UnidentifiedImageError: cannot identify image file
+
+		# struct.error: unpack_from requires a buffer of at least 4 bytes for unpacking 4 bytes at offset 0 (actual buffer size is 0)
+		# followed by:
+		# OSError: image file is truncated
+
+		# SyntaxError: broken PNG file
 
 		# Convert image to raw byte data
 		byte_io = io.BytesIO()
-		PIL.Image.open(os.path.join(image_path, os.listdir(image_path)[0])).save(byte_io, format='PNG')
+
+		try:
+			PIL.Image.open(os.path.join(image_path, os.listdir(image_path)[0])).save(byte_io, format='PNG')
+		except (PIL.UnidentifiedImageError, OSError, SyntaxError):
+			continue
+
 		byte_data = byte_io.getvalue()
 
 		# Use xclip to copy the image to clipboard
 		subprocess.Popen(['xclip', '-selection', 'clipboard', '-t', 'image/png'], stdin=subprocess.PIPE).communicate(input=byte_data)
-		
+
 		# Wait for the button to be clickable
 		if not first:
 			ui.WebDriverWait(driver, 10).until(ec.element_to_be_clickable((by.By.CSS_SELECTOR, "button[aria-label='Clear image']"))).click()
